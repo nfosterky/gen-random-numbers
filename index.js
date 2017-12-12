057711228ba08669de2140edf5c47c09a3b628fe
Nathaniel Foster <nathanielsmusic@gmail.com>
2017-12-12 10:47:17 -0700

    added random letter generator

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
 const generateRandomNumbers = require('./lib/gen-random-numbers.js');
+const generateRandomLetters = require('./lib/gen-random-letters.js');
 
 module.exports = {
-  generateRandomNumbers
+  generateRandomNumbers,
+  generateRandomLetters
 }
